c222bd86af2e6118059ee96493a7ebeeaffef001
AxelAristud <48629529+AxelAristud@users.noreply.github.com>
2020-04-17 23:04:29 -0400

    Add files via upload
    added randomizer and display

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
 import Grid from '@material-ui/core/Grid'
 import { Button, Typography} from '@material-ui/core'
+import axios from 'axios';
 import Fastfood from '@material-ui/icons/Fastfood'
 import { makeStyles } from '@material-ui/styles'
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+
+//card stuff
+import { CardHeader } from '@material-ui/core';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Checkbox from '@material-ui/core/Checkbox';
+
+import Avatar from '@material-ui/core/Avatar'
+
 
 const useStyles = makeStyles(() => ({
     typographyStyles: {
@@ -11,22 +22,105 @@ const useStyles = makeStyles(() => ({
     }
 }));
 
+var x = 0;
+
+//random integer generator for the recieved array from google ♥
+function getRandomInt() {
+    return Math.floor(Math.random() * Math.floor(20));
+  }
+
+
 export class Result extends Component {
 
-    render() {
-        return (
-          <Map google={this.props.google} zoom={14}>
+  
 
-            <Marker onClick={this.onMarkerClick}
-                    name={'Current location'} />
+  handleSubmit = event => {
+    const ops = {
+      method: 'GET',
+      headers: { 'content-type': 'application/json' },
+      url: "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + this.props.latitude + "," + this.props.longitude + "&radius=" + this.props.radius + "&type=restaurant&keyword=" + this.props.foodTypes + "&key=AIzaSyDqBSIr1u7owxnrzuWY-GlULfxeTuz9kIk"
 
-            <InfoWindow onClose={this.onInfoWindowClose}>
-                
-            </InfoWindow>
-          </Map>
-        );
-      }
-    }
+  }
+  axios(ops)
+    .then(res => {
+        // Get the response here, do something with it here
+        // TODO stuff here!
+        //this.props.updateUsername(loginInfo.username);
+        //this.props.nextStep();
+          console.log(res.data);
+          x = getRandomInt()
+          console.log(res.data.results[x].name);
+          console.log(res.data.results[x].vicinity);
+
+          
+          
+    }).catch((error) => {
+        // There was an error sent back, so read the String sent back and act accordingly.
+        console.log(error);
+        if(error  === "Authorization Unsuccessful") {
+            // If we got here, the user's login details were not in the database.
+            console.log("INCORRECT LOGIN DETAILS");
+        }
+        else if(error  === "Authorization Unsuccessful, confirm email") {
+            // If we got here, the user's email was unverifed.
+            console.log("EMAIL UNVERIFIED")
+        }
+        else {
+            // If we got here, some unknown error occured.
+            console.log("SOME UNKNOWN ERROR :(");
+        }
+    });
+  }
+
+
+
+
+  
+  render() {
+    const { handleChange } = this.props
+    const { image, name } = this.props
+
+    return (
+        
+        <Grid container
+            direction='column'
+            justify='center'
+            alignItems='center'
+            spacing={3}
+        >
+            <Grid item />
+            
+            <Grid item container spacing={3} justify='center'>
+
+
+
+                    <Grid item xs={12} sm={7}>
+                    <Button 
+                        variant='contained'
+                        color="secondary"
+                        onClick={this.handleSubmit}
+                        fullWidth={true}
+                    >
+    
+                        Press for magic!
+                    </Button>
+                    </Grid>
+
+                    <Card>
+                    <CardHeader
+                        avatar={<Avatar>{}</Avatar>}
+                        title={<Typography align='center' variant='h5'>{}</Typography>}
+                        
+                    />
+                </Card>
+
+
+            </Grid>
+        </Grid>
+    )
+}
+
+}
 
 
 
